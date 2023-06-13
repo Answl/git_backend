663c4464076ebeaf817a165a882ea178467bdb46
Kim Eun Ji <dms2107@sookmyung.ac.kr>
2023-06-14 02:37:34 +0900

    백엔드개발 1-18. 수정. @Query 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -12,7 +12,7 @@ public class TodoService {
 	private TodoRepository repository;
 	public String testService(){
 		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+		TodoEntity entity = TodoEntity.builder().userId("Kim Eun Ji").title("My first todo item").build();
 		// Todo Entity 저장
 		repository.save(entity);
 		// Todo Entity 검색
